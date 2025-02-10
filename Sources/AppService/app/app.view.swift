7b2471fc865a3cfe1f0bb045c4ade1dc3f1005f6
yaochenfeng <282696845@qq.com>
2025-02-11 03:33:16 +0800

    update platform view

--- Sources/AppService/app/app.view.swift
+++ Sources/AppService/app/app.view.swift
@@ -27,11 +27,33 @@ struct PlatformView<T: NSView>: NSViewRepresentable {
         return base
     }
     
-    func updateNSView(_ nsView: T, context: Context) {
-        
-    }
+    func updateNSView(_ nsView: T, context: Context) {}
     
     typealias NSViewType = T
+    
+    
+    let base: T
+    init(_ base: T) {
+        self.base = base
+    }
+}
+#endif
+
+#if canImport(UIKit)
+import UIKit
+public extension Service where Base: UIView {
+    func asView() -> some View {
+        PlatformView<Base>(base)
+    }
+}
+struct PlatformView<T: UIView>: UIViewRepresentable {
+    func makeUIView(context: Context) -> T {
+        return base
+    }
+    
+    func updateUIView(_ uiView: T, context: Context) {}
+    
+    typealias UIViewType = T
     
     
     let base: T
