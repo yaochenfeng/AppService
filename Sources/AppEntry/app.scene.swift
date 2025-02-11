f8c9a063b5744bbc0b79becd1b61e4f1abcd7e21
yaochenfeng <282696845@qq.com>
2025-02-11 20:41:05 +0800

    add service name

--- Sources/AppEntry/app.scene.swift
+++ Sources/AppEntry/app.scene.swift
@@ -17,44 +17,6 @@ struct MainScene: Scene {
     }
 }
 
-
-
-//public extension ApplicationContext.State {
-//    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-//    var mainScene: WindowGroup<AnyView>? {
-//        get {
-//            return self.getValue("mainScene")
-//        }
-//    }
-//
-//    var mainView: AnyView {
-//
-//
-//        return self.getValue("mainView") ?? AnyView(emptyView)
-//    }
-//
-//    internal var emptyView: some View {
-//        #if canImport(UIKit)
-////        SwiftController.app.launch
-//        Text("import AppEntry dispatch setMainView")
-//        #else
-//        Text("import AppEntry dispatch setMainView")
-//        #endif
-//    }
-//}
-
-
-//extension ApplicationContext.State.Action {
-//    @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-//    public static func setMainScene(_ value: WindowGroup<AnyView>) -> Self {
-//        return .set(key: "mainScene", value: value)
-//    }
-//    @available(iOS 13.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-//    public static func setMainView<Content: View>(@ViewBuilder builder: () -> Content) -> Self {
-//        return .set(key: "mainView", value: AnyView(builder()))
-//    }
-//}
-
 public extension ApplicationContext {
     struct State: ServiceState {
         public enum Action {
